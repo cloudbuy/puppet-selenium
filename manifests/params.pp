ca5d015116ce22aadc402b076307bd219378e136
Damien Churchill <damien.churchill@cloudbuy.com>
2015-08-04 15:43:32 +0100

    don't manage the group under Windows

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,7 +8,6 @@ class selenium::params {
   $user             = 'selenium'
   $manage_user      = true
   $group            = $user
-  $manage_group     = true
   $server_options   = '-Dwebdriver.enable.native.events=1'
   $hub_options      = '-role hub'
   $node_options     = "${server_options} -role node"
@@ -19,14 +18,17 @@ class selenium::params {
 
   case $::osfamily {
     'redhat': {
+      $manage_group     = true
       $install_root     = '/opt/selenium'
       $service_template = 'redhat.selenium.erb'
     }
     'debian': {
+      $manage_group     = true
       $install_root     = '/opt/selenium'
       $service_template = 'debian.selenium.erb'
     }
     'windows': {
+      $manage_group     = false
       $install_root     = 'C:\\Selenium'
       $service_template = 'debian.selenium.erb'
     }
